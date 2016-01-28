8cb5d29829aa3992ec1e23fb8440a997d98a4f8c
Sven Lehnert <mail@sven-lehnert.de>
2016-01-28 21:26:03 +0100

    first working version !

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,8 +1,11 @@
 jQuery(document).ready(function (){
 
+
+
   jQuery(document).on( "change", '.tax_tax_tax', function( evt ) {
 
     var id = jQuery(this).attr('data-id');
+    var taxonomy = jQuery(this).attr('data-taxonomy');
 
     var tax_tax_tax = [];
     jQuery('.tax_tax_tax').each(function() {
@@ -14,7 +17,7 @@ jQuery(document).ready(function (){
     jQuery.ajax({
         type: 'POST',
         url: ajaxurl,
-        data: {"action": "bf_afe_fields_group_create_frontend_form_element_ajax2", "data": tax_tax_tax, 'id': id},
+        data: {"action": "bf_afe_fields_group_create_frontend_form_element_ajax", "data": tax_tax_tax, 'id': id, 'taxonomy': taxonomy},
         beforeSend :function(){
 
         },
--- loader.php
+++ loader.php
@@ -49,7 +49,6 @@ function buddyforms_afe_admin_settings_sidebar_metabox_html(){
     echo '<b>Taxonomy Hierarchically</b><p><a href="#" data-fieldtype="tax-afe" class="bf_add_element_action">Taxonomy</a></p>';
 }
 
-
  /*
   * Create the new Form Builder Form Element for teh ACF Field Groups
   *
@@ -80,12 +79,9 @@ function buddyforms_afe_admin_settings_sidebar_metabox_html(){
  function buddyforms_afe_create_frontend_element($form, $form_args){
      global $buddyforms, $nonce;
 
-echo '<pre>';
-print_r($form_args);
-echo '</pre>';
-
      extract($form_args);
 
+     $customfield_val = explode( ',', $customfield_val );
      $post_type = $buddyforms[$form_slug]['post_type'];
 
      if(!$post_type)
@@ -107,241 +103,65 @@ echo '</pre>';
           $main_cats[$category->term_taxonomy_id] = $category->name;
         }
 
-        $form->addElement( new Element_Select( $customfield['name'], $customfield['slug']. '-select', $main_cats, array( 'class' => 'tax_tax_tax', 'data-id' => $customfield['slug'] )));
+        $form->addElement( new Element_Select( $customfield['name'], $customfield['slug']. '-select', $main_cats, array( 'class' => 'tax_tax_tax', 'data-id' => $id, 'data-taxonomy' => $customfield['taxonomy'] )));
         $form->addElement( new Element_HTML('<div id="taxtax_container">'));
 
-          $terms = wp_get_post_terms( $post_id, $customfield['taxonomy'] );
+        $terms = wp_get_post_terms( $post_id, $customfield['taxonomy'] );
 
+        foreach($terms as $key => $term){
 
+          $term_children =  get_categories(array(
+            'parent' => $term->term_id,
+            'hide_empty' => 0,
+            'taxonomy' => $customfield['taxonomy'],
+            'hierarchical' => false,
+          ));
 
-          foreach($terms as $key => $term){
+          $term_children_array = false;
+          if(is_array($term_children)) :
+            $term_children_array[none] = 'Select One';
+            foreach($term_children as $key => $term_child){
+                $term_children_array[$term_child->term_taxonomy_id] = $term_child->name;
+            }
+          endif;
 
-
-            $term_children =  get_categories(array(
-              'parent' => $term->term_id,
-              'hide_empty' => 0,
-              'taxonomy' => $customfield['taxonomy'],
-              'hierarchical' => false,
-            ));
-
-            echo '<pre>';
-            print_r($term_children);
-            echo '</pre>';
-
+          if(is_array($term_children_array) && count($term_children_array) > 1){
+            $select = new Element_Select('', 'tax_tax_tax[sub][]', $term_children_array, array('class' => 'tax_tax_tax', 'value' => $customfield_val, 'data-id' => $id, 'data-taxonomy' => $customfield['taxonomy'] ));
+            ob_start();
+              $select->render();
+            $select = ob_get_clean();
+            $form->addElement( new Element_HTML( $select ));
           }
-          //$select = new Element_Select('Test ', 'tax_tax_tax[sub][]', $grand_parents_array, array('class' => 'tax_tax_tax'));
 
-        $form->addElement( new Element_HTML('</div>'));
+        }
 
-        $form->addElement( new Element_Hidden( $customfield['slug'], $form_args['customfield_val']) );
+      $form->addElement( new Element_HTML('</div>'));
 
-         break;
-       }
-       return $form;
+      $form->addElement( new Element_Hidden( $customfield['slug'], $form_args['customfield_val']) );
+
+       break;
+     }
+     return $form;
  }
  add_filter('buddyforms_create_edit_form_display_element','buddyforms_afe_create_frontend_element',1,2);
 
 function bf_afe_fields_group_create_frontend_form_element_ajax(){
-
-  if(!isset($_POST['data']))
-    return;
-
-  $parent_id = $_POST['data'];
-
-  $term = get_term_by( 'id', $parent_id, 'product_cat'  ); // get current term
-
-
-
-  $parents = get_term($term->parent, 'product_cat' ); // get parent term
-
-  $children =  get_categories(array(
-     'child_of' => $parent_id,
-     'hide_empty' => 0,
-     'taxonomy' => 'product_cat',
-  ));
-
-  // if has parent and child
-  if(($parents->term_id!="" && sizeof($children)>0)) {
-
-        // echo '<pre>';
-        // print_r($term);
-        // echo '</pre>';
-        // echo '<pre>';
-        // print_r($parents);
-        // echo '</pre>';
-
-        echo '$parents->parent' . $parents->parent . '<br>';
-
-        //echo '$term->parent' . $term->parent . '<br>';
-
-        echo '// if has parent and child';
-
-    //$grand_parent = get_term($parents->parent, 'product_cat' ); // get parent term
-
-
-    do {
-      $grand_parents =  get_categories(array(
-         'child_of' => $parents->parent,
-         'hide_empty' => 0,
-         'taxonomy' => 'product_cat',
-      ));
-
-      $grand_parents_array = false;
-
-      if(is_array($grand_parents)) :
-        $grand_parents_array[none] = 'Select One';
-        foreach($grand_parents as $key => $grand_parent){
-
-            if($grand_parent->parent == $parents->parent)
-                $grand_parents_array[$grand_parent->term_taxonomy_id] = $grand_parent->name;
-        }
-      endif;
-
-      if(is_array($grand_parents_array))
-        $select = new Element_Select('Test ', 'tax_tax_tax[sub][]', $grand_parents_array, array('class' => 'tax_tax_tax'));
-
-      if(is_object($select)) {
-        $select->render();
-      }
-
-      $parents = get_term($parents->parent, 'product_cat' ); // get parent term
-    } while ($parents->parent > 0);
-
-
-
-
-
-
-////7
-    $children_parent =  get_categories(array(
-       'child_of' => $term->parent,
-       'hide_empty' => 0,
-       'taxonomy' => 'product_cat',
-    ));
-
-    $children_parents = false;
-    foreach($children_parent as $key => $child){
-
-        if($child->parent == $term->parent)
-            $children_parents[$child->term_taxonomy_id] = $child->name;
-    }
-
-    if(is_array($children_parents))
-      $select = new Element_Select('Test ', 'tax_tax_tax[sub][]', $children_parents, array('class' => 'tax_tax_tax'));
-
-    if(is_object($select)) {
-      $select->render();
-    }
-
-    $childs = false;
-    foreach($children as $key => $child){
-        if($child->parent == $parent_id)
-            $childs[$child->term_taxonomy_id] = $child->name;
-    }
-
-    if(is_array($childs))
-      $select = new Element_Select('Test ', 'tax_tax_tax[sub][]', $childs, array('class' => 'tax_tax_tax'));
-
-    if(is_object($select)) {
-      $select->render();
-    }
-
-  // else no parent, no child
-  }elseif(($parents->term_id!="") && (sizeof($children)==0)) {
-    // echo '// else no parent, no child';
-    //
-    // do {
-    //   $grand_parents =  get_categories(array(
-    //      'child_of' => $parent_id,
-    //      'hide_empty' => 0,
-    //      'taxonomy' => 'product_cat',
-    //   ));
-    //
-    //   $grand_parents_array = false;
-    //   foreach($grand_parents as $key => $grand_parent){
-    //
-    //       if($grand_parent->parent == $parents->parent)
-    //           $grand_parents_array[$grand_parent->term_taxonomy_id] = $grand_parent->name;
-    //   }
-    //
-    //   if(is_array($grand_parents_array))
-    //     $select = new Element_Select('Test ', 'tax_tax_tax[sub][]', $grand_parents_array, array('class' => 'tax_tax_tax'));
-    //
-    //   if(is_object($select)) {
-    //     $select->render();
-    //   }
-    //
-    //   $grand_parent_loop = get_term($parent_id, 'product_cat' ); // get parent term
-    //   $parent_id = $grand_parent_loop->parent;
-    // } while ($parent_id > 0);
-
-
-    // $children_parent =  get_categories(array(
-    //    'child_of' => $term->parent,
-    //    'hide_empty' => 0,
-    //    'taxonomy' => 'product_cat',
-    // ));
-    //
-    // $children_parents = false;
-    // foreach($children_parent as $key => $child){
-    //
-    //     if($child->parent == $term->parent)
-    //         $children_parents[$child->term_taxonomy_id] = $child->name;
-    // }
-    //
-    // if(is_array($children_parents))
-    //   $select = new Element_Select('Test ', 'tax_tax_tax[sub][]', $children_parents, array('class' => 'tax_tax_tax', 'value' => $parent_id));
-    //
-    // if(is_object($select)) {
-    //   $select->render();
-    // }
-echo 'false';
-    die();
-    return;
-  // elseif no parent, has child
-  }elseif(($parents->term_id=="") && (sizeof($children)>0)) {
-    echo '// elseif no parent, has child';
-      $childs = false;
-      foreach($children as $key => $child){
-          if($child->parent == $parent_id)
-              $childs[$child->term_taxonomy_id] = $child->name;
-      }
-
-      if(is_array($childs))
-        $select = new Element_Select('Test ', 'tax_tax_tax[sub][]', $childs, array('class' => 'tax_tax_tax'));
-
-      if(is_object($select)) {
-        $select->render();
-      } else {
-        echo false;
-      }
-
-  }
-
-  die();
-
-}
-add_action('wp_ajax_bf_afe_fields_group_create_frontend_form_element_ajax', 'bf_afe_fields_group_create_frontend_form_element_ajax');
-
-function bf_afe_fields_group_create_frontend_form_element_ajax2(){
   if(!isset($_POST['data'])){
     echo 'false';
     die();
   }
 
-
-  // print_r($_POST['data']);
-
-  $cats = $_POST['data'];
-  $id   = $_POST['id'];
+  $cats       = $_POST['data'];
+  $id         = $_POST['id'];
+  $taxonomy   = $_POST['taxonomy'];
 
 if(is_array($cats)){
   foreach($cats as $cat_key => $cat){
 
     $childs_of_cat =  get_categories(array(
-       'child_of' => $cat,
+       'child_of'   => $cat,
        'hide_empty' => 0,
-       'taxonomy' => 'product_cat',
+       'taxonomy'   => $taxonomy,
     ));
 
     if ( !$childs_of_cat ) {
@@ -358,121 +178,105 @@ if(is_array($cats)){
     endif;
 
     if(is_array($childs_of_cat_array)){
-
-      // echo '<pre>';
-      // print_r($childs_of_cat_array);
-      // echo '</pre>';
-      //
-      // echo 'cat' . $cat . ' - ';
-      // echo '$cats[$key+1] ' . $cats[$cat_key]. '<br>';
-      $select = new Element_Select('Test ', 'tax_tax_tax', $childs_of_cat_array, array('class' => 'tax_tax_tax', 'value' => $cats[$cat_key+1], 'data-id' => $id ));
+      $select = new Element_Select('Test ', 'tax_tax_tax', $childs_of_cat_array, array('class' => 'tax_tax_tax', 'value' => $cats[$cat_key+1], 'data-id' => $id, 'data-taxonomy' => $taxonomy ));
     }
 
     if(is_object($select)) {
       $select->render();
     }
-
   }
-
 }
 die();
 }
 
-add_action('wp_ajax_bf_afe_fields_group_create_frontend_form_element_ajax2', 'bf_afe_fields_group_create_frontend_form_element_ajax2');
+add_action('wp_ajax_bf_afe_fields_group_create_frontend_form_element_ajax', 'bf_afe_fields_group_create_frontend_form_element_ajax');
 
 function buddyforms_afe_update_post_meta($customfield, $post_id){
 
-  if( $customfield['type'] == 'tax-afe' ){
+  if( $customfield['type'] != 'tax-afe' )
+    return;
 
-    $taxonomy = get_taxonomy($customfield['taxonomy']);
+  $taxonomy = get_taxonomy($customfield['taxonomy']);
 
-          if(isset($customfield['multiple'])) {
+  if(isset($customfield['multiple'])) {
 
-              if (isset($taxonomy->hierarchical) && $taxonomy->hierarchical == true) {
+    if (isset($taxonomy->hierarchical) && $taxonomy->hierarchical == true) {
 
-                  if (isset($_POST[$customfield['slug']]))
-                      $tax_item = $_POST[$customfield['slug']];
+      if (isset($_POST[$customfield['slug']]))
+          $tax_item = $_POST[$customfield['slug']];
 
-                  if ($tax_item[0] == -1 && !empty($customfield['taxonomy_default'])) {
-                      //$taxonomy_default = explode(',', $customfield['taxonomy_default'][0]);
-                      foreach ($customfield['taxonomy_default'] as $key => $tax) {
-                          $tax_item[$key] = $tax;
-                      }
-                  }
-
-                  wp_set_post_terms($post_id, $tax_item, $customfield['taxonomy'], false);
-              } else {
-
-                  $slug = Array();
-
-                  if (isset($_POST[$customfield['slug']])) {
-                      $postCategories = $_POST[$customfield['slug']];
-
-                      foreach ($postCategories as $postCategory) {
-                          $term = get_term_by('id', $postCategory, $customfield['taxonomy']);
-                          $slug[] = $term->slug;
-                      }
-                  }
-
-                  wp_set_post_terms($post_id, $slug, $customfield['taxonomy'], false);
-
-              }
-
-              if (isset($_POST[$customfield['slug'] . '_creat_new_tax']) && !empty($_POST[$customfield['slug'] . '_creat_new_tax'])) {
-                  $creat_new_tax = explode(',', $_POST[$customfield['slug'] . '_creat_new_tax']);
-                  if (is_array($creat_new_tax)) {
-                      foreach ($creat_new_tax as $key => $new_tax) {
-                          $wp_insert_term = wp_insert_term($new_tax, $customfield['taxonomy']);
-                          wp_set_post_terms($post_id, $wp_insert_term, $customfield['taxonomy'], true);
-                      }
-                  }
-
-              }
-          } else {
-              wp_delete_object_term_relationships( $post_id, $customfield['taxonomy'] );
-              if (isset($_POST[$customfield['slug'] . '_creat_new_tax']) && !empty($_POST[$customfield['slug'] . '_creat_new_tax'])) {
-                  $creat_new_tax = explode(',', $_POST[$customfield['slug'] . '_creat_new_tax']);
-                  if (is_array($creat_new_tax)) {
-                      foreach ($creat_new_tax as $key => $new_tax) {
-                          $wp_insert_term = wp_insert_term($new_tax, $customfield['taxonomy']);
-                          wp_set_post_terms($post_id, $wp_insert_term, $customfield['taxonomy'], true);
-                      }
-                  }
-
-              } else {
-
-                  if (isset($taxonomy->hierarchical) && $taxonomy->hierarchical == true) {
-
-                      if (isset($_POST[$customfield['slug']]))
-                          $tax_item = $_POST[$customfield['slug']];
-
-                      if ($tax_item[0] == -1 && !empty($customfield['taxonomy_default'])) {
-                          //$taxonomy_default = explode(',', $customfield['taxonomy_default'][0]);
-                          foreach ($customfield['taxonomy_default'] as $key => $tax) {
-                              $tax_item[$key] = $tax;
-                          }
-                      }
-
-                      wp_set_post_terms($post_id, $tax_item, $customfield['taxonomy'], false);
-                  } else {
-
-                      $slug = Array();
-
-                      if (isset($_POST[$customfield['slug']])) {
-                          $postCategories = $_POST[$customfield['slug']];
-
-                          foreach ($postCategories as $postCategory) {
-                              $term = get_term_by('id', $postCategory, $customfield['taxonomy']);
-                              $slug[] = $term->slug;
-                          }
-                      }
-
-                      wp_set_post_terms($post_id, $slug, $customfield['taxonomy'], false);
-
-                  }
-              }
-
+      if ($tax_item[0] == -1 && !empty($customfield['taxonomy_default'])) {
+          //$taxonomy_default = explode(',', $customfield['taxonomy_default'][0]);
+          foreach ($customfield['taxonomy_default'] as $key => $tax) {
+              $tax_item[$key] = $tax;
           }
+      }
+
+      wp_set_post_terms($post_id, $tax_item, $customfield['taxonomy'], false);
+    } else {
+
+      $slug = Array();
+
+      if (isset($_POST[$customfield['slug']])) {
+          $postCategories = $_POST[$customfield['slug']];
+
+          foreach ($postCategories as $postCategory) {
+              $term = get_term_by('id', $postCategory, $customfield['taxonomy']);
+              $slug[] = $term->slug;
+          }
+      }
+
+      wp_set_post_terms($post_id, $slug, $customfield['taxonomy'], false);
+    }
+    if (isset($_POST[$customfield['slug'] . '_creat_new_tax']) && !empty($_POST[$customfield['slug'] . '_creat_new_tax'])) {
+        $creat_new_tax = explode(',', $_POST[$customfield['slug'] . '_creat_new_tax']);
+        if (is_array($creat_new_tax)) {
+            foreach ($creat_new_tax as $key => $new_tax) {
+                $wp_insert_term = wp_insert_term($new_tax, $customfield['taxonomy']);
+                wp_set_post_terms($post_id, $wp_insert_term, $customfield['taxonomy'], true);
+            }
+        }
+    }
+  } else {
+    wp_delete_object_term_relationships( $post_id, $customfield['taxonomy'] );
+    if (isset($_POST[$customfield['slug'] . '_creat_new_tax']) && !empty($_POST[$customfield['slug'] . '_creat_new_tax'])) {
+        $creat_new_tax = explode(',', $_POST[$customfield['slug'] . '_creat_new_tax']);
+        if (is_array($creat_new_tax)) {
+            foreach ($creat_new_tax as $key => $new_tax) {
+                $wp_insert_term = wp_insert_term($new_tax, $customfield['taxonomy']);
+                wp_set_post_terms($post_id, $wp_insert_term, $customfield['taxonomy'], true);
+            }
+        }
+    } else {
+      if (isset($taxonomy->hierarchical) && $taxonomy->hierarchical == true) {
+
+          if (isset($_POST[$customfield['slug']]))
+              $tax_item = $_POST[$customfield['slug']];
+
+          if ($tax_item[0] == -1 && !empty($customfield['taxonomy_default'])) {
+              //$taxonomy_default = explode(',', $customfield['taxonomy_default'][0]);
+              foreach ($customfield['taxonomy_default'] as $key => $tax) {
+                  $tax_item[$key] = $tax;
+              }
+          }
+
+          wp_set_post_terms($post_id, $tax_item, $customfield['taxonomy'], false);
+      } else {
+
+          $slug = Array();
+          if (isset($_POST[$customfield['slug']])) {
+              $postCategories = $_POST[$customfield['slug']];
+
+              foreach ($postCategories as $postCategory) {
+                  $term = get_term_by('id', $postCategory, $customfield['taxonomy']);
+                  $slug[] = $term->slug;
+              }
+          }
+
+          wp_set_post_terms($post_id, $slug, $customfield['taxonomy'], false);
+      }
+    }
   }
+
 }
 add_action('buddyforms_update_post_meta', 'buddyforms_afe_update_post_meta', 10, 2);
